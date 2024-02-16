3fedc437631a4ee4f74082c1a58355060eccede5
GreenLontsiCodz <130116183+pixelbender77@users.noreply.github.com>
2024-02-16 17:41:06 +0100

    Started and almost done with Despito webapp project.
    Basic UI completed - only footer
    Started with JS.
    
    Hope i'll be done by Tuesday

---
+++ main.js
@@ -0,0 +1,11 @@
+const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2");
+const input3 = document.getElementById("input3");
+const chat_text = document.getElementById("chat-text");
+const actionButton = document.getElementById("action-button");
+//
+//
+function hello() {
+    console.log("Hello World");
+}
+actionButton.addEventListener("click", hello);
